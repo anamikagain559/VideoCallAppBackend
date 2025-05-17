dd4bac4282883600912a4c834ae3e2f49fdcf1a7
anamikagain559 <anamika.zaimahtech@gmail.com>
2025-05-17 13:29:46 +0600

    chat routes added

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser';
 dotenv.config();
 import authRoutes from './routes/auth.route.js';
 import userRoutes from "./routes/user.route.js";
-
+import chatRoutes from "./routes/chat.route.js";
 import { connectDB } from './lib/db.js';
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,6 +13,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/chat",chatRoutes);
+
 app.get("/", (req, res) => {
   res.send("App is Running");
 });
