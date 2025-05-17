3b95fda968d33fe91d8c8b3b6b662c96f4749e3d
anamikagain559 <anamika.zaimahtech@gmail.com>
2025-05-17 13:20:37 +0600

    friend request model done

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,12 +1,18 @@
-import express from 'express';
-import {signup, login,logout, onboard} from '../controllers/auth.controller.js';
-const router = express.Router();
+import express from "express";
+import { login, logout, onboard, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
+const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
-router.get("/onboard",protectRoute,onboard);
+router.post("/onboarding", protectRoute, onboard);
 
-export default router; 
+// check if user is logged in
+router.get("/me", protectRoute, (req, res) => {
+  res.status(200).json({ success: true, user: req.user });
+});
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser'; 
 dotenv.config();
 import authRoutes from './routes/auth.route.js';
+import userRoutes from "./routes/user.route.js";
 
 import { connectDB } from './lib/db.js';
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json());
-
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
-
+app.use("/api/users", userRoutes);
 app.get("/", (req, res) => {
   res.send("App is Running");
 });
